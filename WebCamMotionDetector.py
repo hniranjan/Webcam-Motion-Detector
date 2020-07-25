ce8f6619f74b8d29154a9869cde50d196e60283c
hniranjan <65037594+hniranjan@users.noreply.github.com>
2020-07-25 22:53:00 +0530

    Add files via upload

---
+++ WebCamMotionDetector.py
@@ -0,0 +1,103 @@
+import cv2, time
+import pandas as pd
+ 
+from datetime import datetime 
+  
+ 
+static_back = None
+  
+
+motion_list = [ None, None ] 
+  
+
+time = [] 
+  
+
+df = pd.DataFrame(columns = ["Start", "End"]) 
+  
+
+video = cv2.VideoCapture(0) 
+  
+
+while True: 
+    
+    check, frame = video.read() 
+  
+     
+    motion = 0
+  
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+  
+    
+    gray = cv2.GaussianBlur(gray, (21, 21), 0) 
+  
+    
+    if static_back is None: 
+        static_back = gray 
+        continue
+  
+    
+    diff_frame = cv2.absdiff(static_back, gray) 
+  
+   
+    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1] 
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2) 
+  
+    
+    contsss,_ = cv2.findContours(thresh_frame.copy(),  
+                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+  
+    for x in contsss: 
+        if cv2.contourArea(x) < 10000: 
+            continue
+        motion = 1
+  
+        (x, y, w, h) = cv2.boundingRect(x) 
+        
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) 
+  
+     
+    motion_list.append(motion) 
+  
+    motion_list = motion_list[-2:] 
+  
+    
+    if motion_list[-1] == 1 and motion_list[-2] == 0: 
+        time.append(datetime.now()) 
+  
+    
+    if motion_list[-1] == 0 and motion_list[-2] == 1: 
+        time.append(datetime.now()) 
+  
+   
+    cv2.imshow("Gray_Frame", gray) 
+  
+    
+    cv2.imshow("Diff_Frame", diff_frame) 
+  
+    
+    cv2.imshow("Thres_Frame", thresh_frame) 
+  
+    
+    cv2.imshow("Color_Frame", frame) 
+  
+    key = cv2.waitKey(1) 
+   
+    if key == ord('q'): 
+       
+        if motion == 1: 
+            time.append(datetime.now()) 
+        break
+  
+ 
+for i in range(0, len(time), 2): 
+    df = df.append({"Start":time[i], "End":time[i + 1]}, ignore_index = True) 
+  
+
+df.to_csv("All_Time_of_movements.csv") 
+  
+video.release() 
+  
+
+cv2.destroyAllWindows() 
